9fc4bbef356ec761971f797277321afae3c338d7
Netanel <niko0355@gmail.com>
2026-01-06 20:19:34 +0200

    netanel

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import functions_framework
+from flask import make_response
 
 @functions_framework.http
 def start(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+    # ---- CORS ----
+    if request.method == 'OPTIONS':
+        response = make_response('', 204)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
 
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
+    response_headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+    # ---- Read input ----
+    request_json = request.get_json(silent=True)
+
+    if request_json and 'text' in request_json:
+        text = request_json['text']
     else:
-        name = 'World'
-    return 'Hello {}!'.format(name)
+        return make_response(
+            ('Missing "text" field', 400, response_headers)
+        )
+
+    # ---- Logic ----
+    result = f"Hello {text}!"
+
+    return make_response(result, 200, response_headers)
