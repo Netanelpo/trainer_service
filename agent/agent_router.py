4985d04edc0cb661a63e4d6084e1dd2672061516
Netanel <niko0355@gmail.com>
2026-01-14 13:34:28 +0200

    netanel

--- agent/agent_router.py
+++ agent/agent_router.py
@@ -2,4 +2,10 @@ def agent_route(stage: str) -> str:
     if stage == "LanguageChoiceAgent":
         return "EnglishWordsAgent"
         # return "LearningGoalAgent"
+    elif stage == "EnglishWordsAgent":
+        return "FromEnglishTranslatorAgent"
+    elif stage == "FromEnglishTranslatorAgent":
+        return "ToEnglishTranslatorAgent"
+    elif stage == "ToEnglishTranslatorAgent":
+        return "ReviewerAgent"
     raise ValueError(f"Unknown stage: {stage}")
