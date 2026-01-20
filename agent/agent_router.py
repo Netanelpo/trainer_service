be5602dcc1c956ef654d650079c49667d3f7da01
Netanel <niko0355@gmail.com>
2026-01-20 13:00:03 +0200

    netanel

--- agent/agent_router.py
+++ agent/agent_router.py
@@ -1,6 +1,9 @@
 def get_agent_name(action: str) -> str:
     if action == "SET_WORDS":
         return "EnglishWordsAgent"
+    if action == "EN_TO_TARGET_TRAINING":
+        return "FromEnglishTranslatorAgent"
+
     # if action == "LanguageChoiceAgent":
     #     return "EnglishWordsAgent"
     # elif action == "EnglishWordsAgent":
---
+++ tests/test_words.py
@@ -0,0 +1,87 @@
+def test_no_input(client):
+    resp = client.post(
+        "/",
+        json={
+            'action': 'SET_WORDS',
+            'language': 'Hebrew',
+        },
+    )
+
+    body = resp.get_json()
+    print("BODY", body)
+    assert resp.status_code == 400
+    assert resp.headers["Access-Control-Allow-Origin"] == "*"
+
+    assert body == {'error': "input is required."}
+
+
+def test_no_action(client):
+    resp = client.post(
+        "/",
+        json={
+            'input': 'hi',
+            'action': '',
+            'language': 'Hebrew',
+        },
+    )
+
+    body = resp.get_json()
+    print("BODY", body)
+    assert resp.status_code == 400
+    assert resp.headers["Access-Control-Allow-Origin"] == "*"
+
+    assert body == {'error': "action is required."}
+
+
+def test_no_language(client):
+    resp = client.post(
+        "/",
+        json={
+            'input': 'hi',
+            'action': 'SET_WORDS',
+            'language': '',
+        },
+    )
+
+    body = resp.get_json()
+    print("BODY", body)
+    assert resp.status_code == 400
+    assert resp.headers["Access-Control-Allow-Origin"] == "*"
+
+    assert body == {'error': "language is required."}
+
+
+def test_words_empty(client):
+    resp = client.post(
+        "/",
+        json={
+            'input': 'hi',
+            'action': 'SET_WORDS',
+            'language': 'Hebrew',
+        },
+    )
+
+    body = resp.get_json()
+    print("BODY", body)
+    assert resp.status_code == 200
+    assert resp.headers["Access-Control-Allow-Origin"] == "*"
+    assert body["output"]
+    assert not body.get("words")
+
+
+def test_words(client):
+    resp = client.post(
+        "/",
+        json={
+            'input': 'apple, sleep, go, dance',
+            'action': 'SET_WORDS',
+            'language': 'Hebrew',
+        },
+    )
+
+    body = resp.get_json()
+    print("BODY", body)
+    assert resp.status_code == 200
+    assert resp.headers["Access-Control-Allow-Origin"] == "*"
+    assert body["output"]
+    assert set(body["words"]) == {"apple", "sleep", "go", "dance"}
